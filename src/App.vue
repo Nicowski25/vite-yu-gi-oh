877edff9e6c071ae1c30ad2e9e41f1221e729cb2
Nicowski25 <nicola.faedo0610@gmail.com>
2023-04-17 17:17:29 +0200

    cards api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
-<template>
-  <div>
+<script>
+  import { store } from './store';
 
-  </div>
-</template>
-
-<script setup>
-
+  export default {
+    props: {
+      cards: Object,
+    },
+    data() {
+      return {
+        store
+      }
+    }
+  }
 </script>
+
+<template>
+<!-- header section -->
+  <header>
+    <h1>Yu-Gi-Oh</h1>
+  </header>
+
+  <!-- main section -->
+  <main>
+
+    <div class="container">
+      <div class="row">
+
+        <div class="col" v-for="card in cards">
+          <img :src="card.image" alt="">
+        </div>
+
+      </div>
+    </div>
+
+  </main>
+</template>
 
 <style lang="scss" scoped>
 
