13f52d703738df306dea2f9421bcd71776d720af
Nicowski25 <nicola.faedo0610@gmail.com>
2023-04-18 10:54:22 +0200

    style and js fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import { state } from './state';
-import axios from 'axios';
+
 export default {
 
   data() {
@@ -8,43 +8,33 @@ export default {
       state
     }
   },
-  methods: {
-    fetchCards(url) {
-      axios
-        .get(url)
-        .then(response => {
-          console.log(response);
-          state.cards = response.data.data
-          state.meta = response.data.meta
-        })
-        .catch(err => {
-          console.log(err);
-          console.log(err.message);
-        })
-    }
-  },
   mounted() {
-    this.fetchCards(state.API_URL)
+    this.state.fetchCards(this.state.API_URL)
   }
 }
 </script>
 
 <template>
   <!-- header section -->
-  <header>
-    <h1>Yu-Gi-Oh</h1>
+  <header class="text-center py-4">
+    <h1 class="text-light">Yu-Gi-Oh</h1>
   </header>
 
   <!-- main section -->
   <main>
     <section class="cards">
       <div class="container">
-        <div class="row row-cols-sm-2 g-4">
+        <div class="row row-cols-sm-4 g-4">
   
           <div class="col" v-for="card in state.cards">
-            <div class="card">
-              <img class="img-fluid" :src="card.card_images[0].image_url" alt="">
-  
+            <div class="card h-100">
+              <img class="img-fluid card-img-top" :src="card.card_images[0].image_url" alt="">
+              <div class="card-body text-light">
+                <h3> {{ card.name }} </h3>
+                <div class="ak">
+                  {{ card.archetype }}
+                </div>
+              </div>
             </div>
           </div>
   
@@ -57,4 +47,19 @@ export default {
   </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+header {
+  background-color: #d58f39;
+}
+
+main {
+  background-color: #212428;
+}
+
+.card {
+  background-color: #3b3838a3;
+  filter: blur(0 0 2rem);
+}
+
+</style>
--- src/state.js
+++ src/state.js
@@ -1,8 +1,22 @@
 import { reactive } from "vue";
+import axios from "axios";
 
-export const state = reactive ({
+export const state = reactive({
     loading: '',
     API_URL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0',
     cards: null,
     meta: null,
+    fetchCards(url) {
+        axios
+            .get(url)
+            .then(response => {
+                console.log(response);
+                this.cards = response.data.data
+                this.meta = response.data.meta
+            })
+            .catch(err => {
+                console.log(err);
+                console.log(err.message);
+            })
+    }
 })
